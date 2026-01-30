059f5b09668481ef3a37fa43b39aadd12cc5c482
kartik-mali-m <mali9765kartik@gmail.com>
2026-01-30 18:22:36 +0530

    Add project files.

---
+++ Clint_static wen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clint_static_wen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
